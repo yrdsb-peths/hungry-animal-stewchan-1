660291d6058445d04e393a1d2e7c2bc97e20ad01
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-05-07 08:35:09 -0400

    Add images array

--- MyWorld.java
+++ MyWorld.java
@@ -1,13 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
-{
+{ 
+    GreenfootImage[] images = new GreenfootImage[10];
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
     }
 }
